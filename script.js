8665df8ba3e6f60580692c132bf028c0d67dc8c6
Hittitech <mdadnansami9381@gmail.com>
2024-05-26 20:41:37 +0530

    added navbar

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.getElementById('carouselSlider');
-    const items = document.querySelectorAll('.carousel-item');
-    const sections = document.querySelectorAll('.section');
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
 
-    slider.addEventListener('input', function() {
-        updateCarousel(slider.value);
-        updateSections(slider.value);
-    });
-
-    function updateCarousel(index) {
-        const offset = (index - 1) * -100;
-        items.forEach(item => {
-            item.style.transform = `translateX(${offset}%)`;
-        });
-    }
-
-    function updateSections(index) {
-        sections.forEach((section, i) => {
-            if (i === (index - 1)) {
-                section.classList.add('active');
-            } else {
-                section.classList.remove('active');
-            }
-        });
-    }
-
-    let currentIndex = 1;
-    setInterval(() => {
-        currentIndex = currentIndex < 4 ? currentIndex + 1 : 1;
-        slider.value = currentIndex;
-        updateCarousel(currentIndex);
-        updateSections(currentIndex);
-    }, 3000); 
-});
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
